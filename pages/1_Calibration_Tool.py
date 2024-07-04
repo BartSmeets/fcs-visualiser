512d45cd6f4189e81d736a5dcbfda93465494a85
Bart Smeets <bart.smeets.qsp@kuleuven.be>
2024-07-04 12:08:21 +0200

    format

--- pages/1_Calibration_Tool.py
+++ pages/1_Calibration_Tool.py
@@ -22,11 +22,11 @@ st.write(r"where $t_{1}$ and $t_{2}$ are the times (μs) at which a peak is obse
 with st.container(border=True):
     col1, col2 = st.columns(2)
     with col1:
-        t1 = st.number_input(r'$t_{1}$ (μs)', min_value=0)
-        m1 = st.number_input(r'$m_{1}$ (amu)', min_value=0)
+        t1 = st.number_input(r'$t_{1}$ (μs)', min_value=0.0, format='%.3f')
+        m1 = st.number_input(r'$m_{1}$ (amu)', min_value=0.0, format='%.3f')
     with col2:
-        t2 = st.number_input(r'$t_{2}$ (μs)', min_value=0)
-        m2 = st.number_input(r'$m_{2}$ (amu)', min_value=0)
+        t2 = st.number_input(r'$t_{2}$ (μs)', min_value=0.0, format='%.3f')
+        m2 = st.number_input(r'$m_{2}$ (amu)', min_value=0.0, format='%.3f')
 
 # Output
 with st.container(border=True):
