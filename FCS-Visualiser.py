58acb04fb3da2c7ee7b23f42ad9272aded4a7aaf
Bart Smeets <bart.smeets.qsp@kuleuven.be>
2025-03-25 13:20:51 +0100

    Simplify Main App

--- FCS-Visualiser.py
+++ FCS-Visualiser.py
@@ -15,7 +15,8 @@ import os.path
 # Page Config
 st.set_page_config(
     page_title="FCS Visualiser",
-    page_icon="https://static-00.iconduck.com/assets.00/python-icon-512x509-pb65l7gl.png")
+    page_icon="https://static-00.iconduck.com/assets.00/python-icon-512x509-pb65l7gl.png",
+    layout='wide')
 st.write("# FCS Visualiser")
 try:
     with open('defaults.toml', 'r') as f:
@@ -28,8 +29,6 @@ except OSError:
 # Initialise session states
 if 'directory' not in st.session_state:
     st.session_state['directory'] = defaults['directory']
-if 'prominence' not in st.session_state:
-    st.session_state['prominence'] = 1
 if 'a' not in st.session_state:
     st.session_state['a'] = defaults['calibration']['a']
 if 'k' not in st.session_state:
@@ -38,8 +37,6 @@ if 'data' not in st.session_state:
     st.session_state['data'] = []
 if 'old_data' not in st.session_state:
     st.session_state['old_data'] = []
-if 'calibration_data' not in st.session_state:
-    st.session_state['calibration_data'] = None
 if 'baseline' not in st.session_state:
     st.session_state['baseline'] = None
 if 'dataframe' not in st.session_state:
@@ -66,19 +63,22 @@ def select_folder():
 with st.sidebar:
     # Buttons
     col1, col2 = st.columns(2)
+    ## Select Directory
     with col1:
         if st.button("Select Directory"):
             selected = select_folder()
             if selected != '':
                 st.session_state['directory'] = selected
+    ## Refresh Files
     with col2:
         if st.button("Refresh Files"):
             file_location = st.session_state['directory'] + "//*" + file_extension
             all_files = glob.glob(file_location)
+    ## Print/Edit Directory
     st.session_state['directory'] = st.text_input("Directory", value=st.session_state['directory'])
     directory_length = len(st.session_state['directory'])
     file_location = st.session_state['directory'] + "//*" + file_extension
-    all_files = glob.glob(file_location)
+    all_files = glob.glob(file_location)    # Load files
     	
 
 # Data Selection
@@ -87,48 +87,24 @@ def gen_df():
     init_param = [st.session_state['a'], st.session_state['k']]
     st.session_state['dataframe'] = pd.DataFrame({'time': [],
                                                 'mass': [],
-                                                'mass_element': [],
                                                 'voltage': [],
-                                                'norm': [],
                                                 'name': []})  
     for name in st.session_state['data']:
         directory = all_files[0][:directory_length+6] + name
-        data = load_data(directory, 'Co', init_param, prominence=st.session_state['prominence'], lam=lam, multiplier=multiplier, baseline_data=st.session_state['baseline'])
         try:
-            data = load_data(directory, 'Co', init_param, prominence=st.session_state['prominence'], lam=lam, multiplier=multiplier, baseline_data=st.session_state['baseline'])
+            data = load_data(directory, init_param)
             df = pd.DataFrame({'time': data.time,
                         'mass': data.mass,
-                        'mass_element': data.mass_element,
                         'voltage': data.voltage,
-                        'norm': data.norm,
                         'name': [name]*len(data.time)})
         except OSError:
             return
-        
-        if name == st.session_state['data'][0]:
-            characterisation, table = data.characterise()
-            characterisation = characterisation[:, 0]
-            st.session_state['table'] = table
-            st.session_state['characterisation'] = characterisation
-            st.session_state['peaks'] = data.peaks
-            st.session_state['calibration_data'] = data
 
-        if hasattr(data, 'baseline'):
+        if st.session_state['baseline'] is not None:
+            data.baseline_correction(lam, multiplier, st.session_state['baseline'])
             df['baseline'] = data.baseline
            
         st.session_state['dataframe'] = pd.concat([st.session_state['dataframe'], df])
-            
-# Prominence Slider and Calibration Parameters
-def optimise():
-    st.session_state['a'], st.session_state['k'] = st.session_state['calibration_data'].optimise(
-            st.session_state['a'], st.session_state['k'], threshold=0.001)
-    try:
-        st.session_state['a'], st.session_state['k'] = st.session_state['calibration_data'].optimise(
-            st.session_state['a'], st.session_state['k'], threshold=0.001)
-    except ValueError:
-        st.warning('No peaks detected for optimisation')
-    except AttributeError:
-        st.warning('No data selected')
 
 # Save Data
 def save():
@@ -151,26 +127,20 @@ def save():
         to_save.loc[:, 'voltage'] = -to_save['voltage']
         np.save(directory + name + '.npy', to_save.to_numpy())
 
-
+# Mass Calibration
 with st.sidebar:
-    # Peak Detection and Mass Calibration
     with st.container(border=True):
-        dummy = np.arange(1, 10, 1)
-        prominence_steps = np.concatenate((dummy/1000, dummy/100, dummy/10, dummy, dummy*10))
-
-        st.write("## Peak Detection & Mass Calibration")
-        st.session_state['prominence'] = st.select_slider("Peak Prominence", options=prominence_steps, value=1)
         st.write('### Mass Calibration')
         st.write('$m = a(t-k)^2$')
 
+        # Show and apply a and k parameters
         col1, col2 = st.columns(2)
         with col1:
             st.session_state['a'] = st.number_input("a", min_value=0.0, step=1e-8, value=st.session_state['a'], format='%.8f')
-            st.button('Auto-Calibrate', on_click=optimise, disabled=True)
-                
         with col2:
             st.session_state['k'] = st.number_input("k", step=1e-8, value=st.session_state['k'], format='%.8f')
-            st.button('Apply', on_click=gen_df)
+        # Apply Button
+        st.button('Apply', on_click=gen_df)
 
     # Baseline Correction
     with st.container(border=True):
@@ -214,54 +184,33 @@ def generate_fig():
             legend_title_text='',
             xaxis=dict(showgrid=True),
             uirevision=True)
-        
-    def show_peak_lines(index):
-        if peak_detection or show_tag:
-            for i, peak in enumerate(st.session_state['peaks']):
-                dataframe = st.session_state['dataframe']
-                x_axis = dataframe[index]
-                x_axis = x_axis[dataframe['name'] == dataframe['name'].iloc[0]]
-                if 'Ar' in st.session_state['characterisation'][i] and show_tag:
-                    fig.add_vline(x_axis.iloc[peak], line_dash="dash", line_color='blue', opacity=0.25)
-                elif peak_detection:
-                    fig.add_vline(x_axis.iloc[peak], line_dash='dot', line_color='grey', opacity=0.25)
     
-    if normalise:
-        y_axis = 'norm'
-    else:
-        y_axis = 'voltage'
-    
+    # Time Spectrum
     if spectrum_type == 'time':
-        fig = px.line(st.session_state['dataframe'], x='time', y=y_axis, color='name')
+        fig = px.line(st.session_state['dataframe'], x='time', y='voltage', color='name')
         prepare_axes('time (us)', 'accumulated voltage (V)')
-        show_peak_lines(spectrum_type)
+    # Mass Spectrum
     elif spectrum_type == 'mass':
-        fig = px.line(st.session_state['dataframe'], x='mass', y=y_axis, color='name')
+        fig = px.line(st.session_state['dataframe'], x='mass', y='voltage', color='name')
         prepare_axes('mass (amu)', 'accumulated voltage (V)')
-        show_peak_lines(spectrum_type)
-    else:
-        fig = px.line(st.session_state['dataframe'], x='mass_element', y=y_axis, color='name')
-        prepare_axes('mass (Co)', 'accumulated voltage (V)')
-        show_peak_lines(spectrum_type)
     
+    # Add Pointer
     if pointer:
         fig.add_vline(pointer_value, line_dash="dash", line_color="red")
     
     return fig
 
-    
+
+# Visualise Data
 if st.session_state['data'] != []:
-    ## Input
+    
+    ## Inputs (X-axis)
     col1, col2 = st.columns([3, 1])
     with col2:
         SPECTRUM_DICT = {'Time': 'time',
-                         'Mass (amu)': 'mass',
-                         'Mass (Co)': 'mass_element'}
-        spectrum_type = st.selectbox('Spectrum Type', ['Time', 'Mass (amu)', 'Mass (Co)'])
+                         'Mass': 'mass'}
+        spectrum_type = st.radio('Spectrum Type', ['Mass','Time'])
         spectrum_type = SPECTRUM_DICT[spectrum_type]
-        peak_detection = st.toggle('Peak Detection')
-        show_tag = st.toggle('Show Tag')
-        normalise = st.toggle('Normalise')
 
         with st.container(border = True):
             pointer = st.toggle('Pointer')
@@ -269,6 +218,7 @@ if st.session_state['data'] != []:
                                             st.session_state['dataframe'][spectrum_type].iloc[0], 
                                             st.session_state['dataframe'][spectrum_type].iloc[-1], 
                                             label_visibility='collapsed')  
+    ## Figure
     with col1:
         fig = generate_fig()
         st.session_state['figure'] = fig
--- modules/calibration.py
+++ modules/calibration.py
@@ -14,22 +14,15 @@ class load_data:
     Provides intuitive access to the different axes and properties (e.g. self.time instead of data[:, 0])
     '''
 
-    def __init__(self, file, element, init_param, prominence=0.2, lam=1e9, multiplier=1, baseline_data=None):
+    def __init__(self, file, init_param):
         '''
         Load the data and perform a first calibration
 
         ### ARGUMENTS:
         - file: path to the data file that will be loaded
-        - prominence (default=0.2): prominence threshold for the peak detection. \\
-            Prominence is the effective height of a peak -> from lowest point to highest point
-        - baseline_end (default=True): boolean to choose whether the baseline correction should be calculated from the last data points of the spectrum \\
-            This must be set to false if the actual signal reaches the border of the measurment window
+        - lam: smoothness parameter for baseline correction
+        - 
         '''
-        # Select element and read the mass dictionary
-        global ELEMENT, MASS_DICT, MASS_ELEMENT
-        ELEMENT = element
-        MASS_DICT = np.load('masses.npy', allow_pickle=True).item()
-        MASS_ELEMENT = MASS_DICT[ELEMENT + "1-"]
 
         # Read data
         self.file = file
@@ -38,8 +31,24 @@ class load_data:
         time = load[:, 0] * 1e6    # us
         self.voltage = -load[time>=0, 1]
         self.time = time[time>=0]
-        self.peaks, self.properties = signal.find_peaks(self.voltage, prominence=prominence)
         self.calibrate(*init_param)
+        
+        return
+
+
+    def calibrate(self, G, t_off):
+        '''
+        Performs a mass calibration, i.e., convert the time axis into a mass axis
+
+        ### ARGUMENTS:
+        - G: First order coefficient of the Taylor expansion
+        - t_off: Time offset <- Inability to locate the exact extraction timing
+        '''
+        self.mass = G * (self.time - t_off)**2
+        return
+
+    
+    def baseline_correction(self, lam=1e9, multiplier=1, baseline_data=None):
         # Baseline correction
         if baseline_data != None:
             def LSS(y, lam):
@@ -55,230 +64,6 @@ class load_data:
             
             baseline = np.load(baseline_data)
             baseline = -multiplier * baseline[time>=0, 1]
-            self.baseline = LSS(baseline, lam)
+            self.baseline = LSS(baseline, lam)  # Smoothen Baseline Measurement
             self.voltage = self.voltage - self.baseline
-        # Normalisation
-        self.norm = self.voltage / np.sum(self.voltage)
-        return
-
-
-    def calibrate(self, G, t_off):
-        '''
-        Performs a mass calibration, i.e., convert the time axis into a mass axis
-
-        ### ARGUMENTS:
-        - G: First order coefficient of the Taylor expansion
-        - t_off: Time offset <- Inability to locate the exact extraction timing
-        '''
-        self.mass = G * (self.time - t_off)**2
-        self.mass_element = self.mass / MASS_ELEMENT
-        return
-
-
-    def optimise(self, G0, t0, threshold=0.001):
-        '''
-        Performs an optimisation routine (minimise Chi^2) to determine the optimal calibration parameters
-
-        ### ARGUMENTS:
-        - p0 (default=[0.09548259, 0.23805761]): The initial guess for the calibration parameters
-        - threshold (default=0.001): Threshold for ignoring a peak: \\
-            Ignoring the peak should improve the chi^2 by more than the threshold
-        '''
-
-        def chi2_G(G, t_off, calibration_peaks, calibration_mass):
-            '''
-            Calibrates the spectrum for the given parameters and calculates the corresponding Chi^2
-
-            ### ARGUMENTS:
-            - G: the variable G calibration parameter
-            - t_off: The fixed time offset
-            - calibration_peaks: the peak indices that will be considered during the optimisation
-            - calibration_mass: the target (exact) mass that should correspond to each peak, e.g., the atom peak should have exactly 1*m_element
-            '''
-            self.calibrate(G, t_off)
-            chi2 = np.sum((self.mass[calibration_peaks] - calibration_mass)**2 / calibration_mass)
-            return chi2
-        
-        def chi2_t(t_off, G, calibration_peaks, calibration_mass):
-            '''
-            Calibrates the spectrum for the given parameters and calculates the corresponding Chi^2
-
-            ### ARGUMENTS:
-            - t_off: The variable time offset
-            - G: the fixed G calibration parameter
-            - calibration_peaks: the peak indices that will be considered during the optimisation
-            - calibration_mass: the target (exact) mass that should correspond to each peak, e.g., the atom peak should have exactly 1*m_element
-
-            ### RETURNS:
-            - G_opt: the optimal G calibration parameter
-            - t_opt: the optimal time offset parameter
-            '''
-            self.calibrate(G, t_off)
-            chi2 = np.sum((self.mass[calibration_peaks] - calibration_mass)**2 / calibration_mass)
-            return chi2
-        
-        ## Identify atom and dimer
-        ### Prepare local datastructure
-        atom = np.zeros(len(self.peaks))
-        dimer = np.zeros(len(self.peaks))
-        
-        ### Identify the candidate peaks -> peaks nearby 1 element mass and 2 element masses
-        atom_options = np.abs(self.mass_element[self.peaks] - 1) < 0.25
-        dimer_options = np.abs(self.mass_element[self.peaks] - 2) < 0.25
-        ### Find the corresponding peak prominences
-        atom[atom_options] = self.properties['prominences'][atom_options]
-        dimer[dimer_options] = self.properties['prominences'][dimer_options]
-        ### The most prominenent peak is likely the atom or the dimer
-        self.atom_peak = self.peaks[atom.argmax()]
-        self.dimer_peak = self.peaks[dimer.argmax()]
-
-        # Prepare calibration data
-        ## Characterise each peak
-        result, _ = self.characterise()
-        peak_names = result[:, 0]
-        ## Filter the type of cluster used as calibration data
-        mask = [name.startswith(ELEMENT) and name.endswith('-') for name in peak_names] # Only the pure clusters
-        ## Compile the calibration data
-        calibration_mass = [MASS_DICT[x] for x in peak_names[mask]]
-        calibration_peaks = self.peaks[mask]
-
-        # Prepare G optimisation
-        to_del = np.zeros(len(calibration_peaks), dtype=bool)
-        opt = minimize(chi2_G, x0=G0, args=(t0, calibration_peaks, calibration_mass))
-        benchmark = opt.fun
-        G_opt = opt.x
-        print('m = a(t-k)^2 \
-              \na0 = ' + str(G_opt) + 'u/us2 \
-              \nk0 = ' + str(t0) + 'us \
-              \n chi2 = ' + str(benchmark))
-
-        # Iteratively delete all peaks and check if it improves the chi2 more than the threshold
-        ## If it does, the peak was not well characterised; if not undelete the peak and continue
-        to_del = np.zeros(len(calibration_peaks), dtype=bool)
-        for i, peak in enumerate(calibration_peaks):
-            # Never delete the atom and dimer
-            if peak == self.atom_peak or peak == self.dimer_peak:
-                continue
-            # Delete
-            to_del[i] = True
-            test_peaks = np.delete(calibration_peaks, to_del)
-            test_mass = np.delete(calibration_mass, to_del)
-            # Minimise Chi2
-            opt = minimize(chi2_G, x0=G_opt, args=(t0, test_peaks, test_mass))
-            opt_chi2 = opt.fun
-            # Delete or retrieve the peak
-            if np.abs(opt_chi2 - benchmark) > threshold:
-                print('deleted index: ' + str(i))
-                benchmark = opt_chi2    # Update the benchmark
-                G_opt = opt.x   # Update the optimal parameter
-            else:
-                to_del[i] = False
-
-        print('\nAfter \'a\' optimisation \
-              \na0 = ' + str(G_opt) + 'u/us2 \
-              \nk0 = ' + str(t0) + 'us \
-              \n chi2 = ' + str(benchmark) + '\
-              \n')
-        
-        # Fix G and repeat for t_off
-        opt = minimize(chi2_t, x0=t0, args=(G_opt, calibration_peaks, calibration_mass))
-        benchmark = opt.fun
-        t_opt = opt.x
-        print('m = a(t-k)^2 \
-              \na0 = ' + str(G_opt) + 'u/us2$ \
-              \nk0$= ' + str(t_opt) + 'us\
-              \n chi2 = ' + str(benchmark) + '\
-              \n')
-
-        # Iteratively delete all peaks and check if it improves the chi2 more than the threshold
-        ## If it does, the peak was not well characterised; if not undelete the peak and continue
-        to_del = np.zeros(len(calibration_peaks), dtype=bool)
-        for i, _ in enumerate(calibration_peaks):
-            # Never delete the atom and dimer
-            if peak == self.atom_peak or peak == self.dimer_peak:
-                continue
-            # Delete
-            to_del[i] = True
-            test_peaks = np.delete(calibration_peaks, to_del)
-            test_mass = np.delete(calibration_mass, to_del)
-            # Minimise Chi2
-            opt = minimize(chi2_t, x0=t_opt, args=(G_opt, test_peaks, test_mass))
-            opt_chi2 = opt.fun
-            # Delete or retrieve the peak
-            if np.abs(opt_chi2 - benchmark) > threshold:
-                print('deleted index: ' + str(i))
-                benchmark = opt_chi2    # Update the benchmark
-                t_opt = opt.x   # Update the optimal parameter
-            else:
-                to_del[i] = False
-
-        print('m = a(t-k)^2 \
-              \na0 = ' + str(G_opt) + 'u/us2$ \
-              \nk0$= ' + str(t_opt) + 'us \
-              \n chi2 = ' + str(benchmark) + '\
-              \n')
-        
-        return G_opt[0], t_opt[0]
-    
-
-    def characterise(self, precision=0.01):
-        '''
-        Characterise the peaks
-
-        ### ARGUMENTS:
-        - precision (default = 0.01): the precision with which the measured mass must overlap the actual mass in order to be characterised
-
-        ### RETURNS:
-        - matched_elements: array containing the characterised elements corresponding to each peak
-        - matched_similarity: array containing the percentile similarity between meased mass and a material. \\
-            similarity is defined as m1/m2, where m2>m1
-        '''
-
-        # Prepare data structure
-        matched_elements = []
-        matched_similarity = []
-        df = pd.DataFrame()
-        df["Mass (amu)"] = self.mass[self.peaks]
-        df["Mass"] = self.mass_element[self.peaks]
-        df["Time (us)"] = self.time[self.peaks]
-
-        # Check every peak
-        for i, peak in enumerate(self.peaks):
-            # Calculate the similarity with each material in the mass dictionary
-            mass = self.mass[peak]
-            similarity = np.minimum(mass, list(MASS_DICT.values())) / np.maximum(mass, list(MASS_DICT.values()))
-            # Compare the similarities to the precision
-            ## Find best match
-            if True in (1-similarity < precision):
-                index = similarity.argmax()
-                name = list(MASS_DICT.keys())[index]
-                matched_elements.append(name)
-                matched_similarity.append(similarity[index])
-            else:
-                matched_elements.append('')
-                matched_similarity.append(0)
-            ## Add other matches to a general table
-            if True in (similarity>(1-precision)):
-                # Find all names and similarities where the element is considered a match
-                mask = similarity>(1-precision)
-                names = np.array(list(MASS_DICT.keys()))[mask]
-                similarities = similarity[mask]
-                # Sort the similarities: most similar (largest value) first
-                sort_index = np.argsort(similarities)[::-1]
-                similarities = similarities[sort_index]
-                names = names[sort_index]
-                # Add to columns
-                for j, (n, s) in enumerate(zip(names, similarities)):
-                    df.loc[i, 'Element ' + str(j)] = n
-                    df.loc[i, "Similarity " + str(j)] = s
-        
-        # Create a nice dataframe for viewing
-        max_result = np.empty((len(self.peaks), 2), dtype=object)
-        max_result[:, 0] = matched_elements
-        max_result[:, 1] = matched_similarity
-        return max_result, df
-    
-
-    def normalise(self):
-        
-        return self.norm
+            return
